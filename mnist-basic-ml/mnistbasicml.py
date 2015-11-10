f0bb29a286acd9e4107b8beebe6c17d5a0f0ea8c
Carl R. Knox <knox.carl@gmail.com>
2015-11-10 09:52:07 -0700

    mnistbasicml now does stochastic training and prints accuracy of the model on the test data.

--- mnist-basic-ml/mnistbasicml.py
+++ mnist-basic-ml/mnistbasicml.py
@@ -50,5 +50,16 @@ train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 # now initialize the created variables
 init = tf.initialize_all_variables()
 
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
